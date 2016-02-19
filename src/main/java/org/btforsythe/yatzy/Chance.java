6f373ab349096d6441ef55193b31be4a33d47ffc
br8b4w5 <brian_forsythe@gap.com>
2016-02-19 14:07:57 -0500

    first Chance test

---
+++ src/main/java/org/btforsythe/yatzy/Chance.java
@@ -0,0 +1,8 @@
+package org.btforsythe.yatzy;
+
+public class Chance implements ScoringStrategy {
+	@Override
+	public int compute(int... rolls) {
+		return 0;
+	}
+}
---
+++ src/test/java/org/btforsythe/yatzy/ChanceTest.java
@@ -0,0 +1,20 @@
+package org.btforsythe.yatzy;
+
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class ChanceTest {
+
+	@Test
+	public void shouldComputeAScore() {
+		
+		ScoringStrategy underTest = new Chance();
+		
+		int result = underTest.compute();
+		
+		assertThat(result, is(greaterThanOrEqualTo(0)));
+	}
+}
