2107b0c320d6a1dfbff6df88104036b00b21dd43
br8b4w5 <brian_forsythe@gap.com>
2016-02-19 14:14:31 -0500

    refactored Chance tests

--- src/test/java/org/btforsythe/yatzy/ChanceTest.java
+++ src/test/java/org/btforsythe/yatzy/ChanceTest.java
@@ -4,14 +4,20 @@ import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ChanceTest {
 
+	private ScoringStrategy underTest;
+
+	@Before
+	public void setup() {
+		underTest = new Chance();		
+	}
+	
 	@Test
 	public void shouldComputeAScore() {
-		
-		ScoringStrategy underTest = new Chance();
 		
 		int result = underTest.compute();
 		
@@ -21,8 +27,6 @@ public class ChanceTest {
 	@Test
 	public void shouldComputeSimpleScore() {
 		
-		ScoringStrategy underTest = new Chance();
-		
 		int dieRoll = 42;
 		int result = underTest.compute(dieRoll);
 		
@@ -31,8 +35,6 @@ public class ChanceTest {
 	
 	@Test
 	public void shouldComputeScoreAsSum() {
-		
-		ScoringStrategy underTest = new Chance();
 		
 		int[] dieRollsSummingTo6 = {1, 2, 3};
 		int result = underTest.compute(dieRollsSummingTo6);
